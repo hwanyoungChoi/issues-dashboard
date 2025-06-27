a023a436b4ab699c98cc162e061af84fc2fc7e27
Cony Choi <welcomechoi94@gmail.com>
2025-06-28 00:22:56 +0900

    feat: 필요한 github api 요청 함수 및 타입들 정의

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -16,3 +16,60 @@ export async function getIssues(
   );
   return data;
 }
+
+export type GetSearchIssuesParams =
+  RestEndpointMethodTypes["search"]["issuesAndPullRequests"]["parameters"];
+export type GetSearchIssuesResponse =
+  RestEndpointMethodTypes["search"]["issuesAndPullRequests"]["response"];
+
+export async function searchIssues(
+  params: GetSearchIssuesParams
+): Promise<GetSearchIssuesResponse["data"]> {
+  const { data } = await octokit.request("GET /search/issues", params);
+  return data;
+}
+
+export type GetIssueParams =
+  RestEndpointMethodTypes["issues"]["get"]["parameters"];
+export type GetIssueResponse =
+  RestEndpointMethodTypes["issues"]["get"]["response"];
+
+export async function getIssue(
+  params: GetIssueParams
+): Promise<GetIssueResponse["data"]> {
+  const { data } = await octokit.request(
+    "GET /repos/{owner}/{repo}/issues/{issue_number}",
+    params
+  );
+  return data;
+}
+
+export type PostIssueParams =
+  RestEndpointMethodTypes["issues"]["create"]["parameters"];
+export type PostIssueResponse =
+  RestEndpointMethodTypes["issues"]["create"]["response"];
+
+export async function postIssue(
+  params: PostIssueParams
+): Promise<PostIssueResponse["data"]> {
+  const { data } = await octokit.request(
+    "POST /repos/{owner}/{repo}/issues",
+    params
+  );
+  return data;
+}
+
+export type PatchIssueParams =
+  RestEndpointMethodTypes["issues"]["update"]["parameters"];
+export type PatchIssueResponse =
+  RestEndpointMethodTypes["issues"]["update"]["response"];
+
+export async function patchIssue(
+  params: PatchIssueParams
+): Promise<PatchIssueResponse["data"]> {
+  const { data } = await octokit.request(
+    "PATCH /repos/{owner}/{repo}/issues/{issue_number}",
+    params
+  );
+  return data;
+}
